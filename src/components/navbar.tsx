a4e286e390acf936c4c8a39ce7cb4473c190b2bb
Smit <smitbhoraniya11@gmail.com>
2023-01-19 21:33:19 +0530

    navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import { Link } from 'react-router-dom'
+import '../styles/navbar.css'
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>Navbar page</h1>
+        <div className='container'>
+            <div className='nav'>
+                <div className='logo_container'>
+                    <h1>Logo</h1>
+                </div>
+                <ul className='nav_items'>
+                    <li><Link className='text' to='/'>Home</Link></li>
+                    <li className="dropdown">
+                        <div className="dropbtn">Dropdown</div>
+                        <div className="dropdown-content">
+                            <a href="#">Link 1</a>
+                            <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
+                        </div>
+                    </li>
+                    <li><Link className='text' to='/contactus'>Contact Us</Link></li>
+                </ul>
+            </div>
         </div>
     )
 }
