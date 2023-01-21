10be9ebcb5723a45be9a77b68fc13528e474951a
Smit <smitbhoraniya11@gmail.com>
2023-01-21 18:11:49 +0530

    grid

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,6 +8,23 @@ const Home = () => {
             <div className='quote'>
                 <h1 className='q'>BEST IN KAPURTHALA</h1>
             </div>
+            <div className='center_container'>
+                <div className='img_container'>
+                    <div className='img_cont'>
+                        <img className='img' src='https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80' />
+                        <h1 className='img_header'>ABC</h1>
+                    </div>
+                    <div className='img_cont'>
+                        <img className='img' src='https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80' />
+                    </div>
+                    <div className='img_cont'>
+                        <img className='img' src='https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80' />
+                    </div>
+                    <div className='img_cont'>
+                        <img className='img' src='https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80' />
+                    </div>
+                </div>
+            </div>
             <div className='aboutus_container'>
                 <h1 className='aboutus'>About Us</h1>
                 <p className='par'>Smart solutions is the best computer/laptop repair shops in Kapurthala. We repair hardware of computer and fix computers PCs, laptops, Printers, at affordable rates in Kapurthala. We also offer quality and on time repairs without wasting your valuable time.</p>
