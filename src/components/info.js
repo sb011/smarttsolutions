0da1a78f8bb48fc65d823c42ca6177ba1b834968
Smit <smitbhoraniya11@gmail.com>
2023-01-22 00:05:41 +0530

    design added

---
+++ src/components/info.js
@@ -0,0 +1,28 @@
+const info = [
+    {
+        id: 1,
+        title: 'PC/LAPTOP REPAIR',
+        info: 'SMART SOLUTIONS, is only one renowned company in Kapurthala to provide best Onsite/Doorsteps repair services and give you highly satisfaction to its clients. Having 20 years huge experience in this field to repair any kind of PC/Laptop like Lenovo, Acer, Vaio, Dell, Toshiba, Mac, Compaq, Sony and IBM, Samsung and other brands also.',
+        img: 'https://lh6.googleusercontent.com/EbV3oeJZ3YDj9ATtoZWLwFuZdIV1PKpXq3x9tff8-PUU0xryhbmHxD1qpM89h7bHr8fb86vlwMkayg-10DYLYQQoALTsi-MVOnaerQGw54LLG71y4xABq9GTaVis26hCXOAeICcdvNEJ3k_j1l_wCySztP1k2JQPI_dqrJHBAr-7qzMntU4_Pg=w1280'
+    },
+    {
+        id: 2,
+        title: 'PRINTER REPAIR',
+        info: 'SMART SOLUTIONS, intends well to avail quality services committed to Computer and Printer tech support. To solve low quality printing, printer paper jam, printer installation and many others, the company means to assure its clients leave away their problems to get their printers functions well.',
+        img: 'https://lh3.googleusercontent.com/QVnUfn2aIXRka00d2klPVbcEV53jAGyVpiJwy9sujj-fpb3SOwdrUKzz4DSy-ELbF6NiJw4madlbQGq_ADIwQ3NoKQEtoOZYxU_2uuPedyuUWd2sNycxHpRV4k4_0XpvuiQ0zMggqsoYCemqqOntNgFBA400mgeVEQg61y3MO2PkW7QJbzsD=w1280',
+    },
+    {
+        id: 3,
+        title: 'CCTV INSTALLATION',
+        info: 'SMART SOLUTIONS, provides door to door service for installation of CCTV Cameras. Best quality cameras and best service all at AFFORDABLE prices. All type of services are provided for Home, Schools, Offices or any other places. Just make a call!!',
+        img: 'https://lh4.googleusercontent.com/JQZgWn5Of8NCNHt--OQ2pATrSvNTAJLp-EyhgihBIDr91PVeZiyq6Zd0bqPjTkMZEMm8dfuGegfar1nUStoAVVWWXPUFZUkjLY7SVPtXqx1cHktOCECpTXKQ068aWORsccjNhq2P0euu38xa3jlpvT85lUkMc9dF4I8syuj8etmjpci_CnGN6g=w1280',
+    },
+    {
+        id: 4,
+        title: 'SUPPORT & AMC',
+        info: 'SMART SOLUTIONS is currently providing services in the Field of Assembled Computers, Computer Peripherals, Networking, Troubleshooting (Repairing), Annual Maintenance Contracts (AMC), Networking etc. We are dedicated to solve your problems. There will be no next door, once you reach us. Committed to solve all your Computer Problems.',
+        img: 'https://lh4.googleusercontent.com/-DLXpCORurIUtVvWAUdfKsfsvuLVh9UhPBWDTzygz12EvNRlPJj_Y6TFyE528w2G4z9lo2CG6uotRblQab70NasUlvxPEW30mfv6fncxflonZWP8hMdlu4AWGkhwPtpvthqdeZj7cNE5mUd4ljhn88OyU-PVHY2ZYdzIrA2ammuSB5eB54bmyg=w1280',
+    },
+]
+
+export default info
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,41 +1,54 @@
 import '../styles/home.css'
-import Header from '../components/header'
+import info from '../components/info'
 
 const Home = () => {
     return (
         <div className='main'>
-            <Header />
-            <div className='quote'>
-                <h1 className='q'>BEST IN KAPURTHALA</h1>
-            </div>
             <div className='center_container'>
                 <div className='img_cont1'>
                     <img className='img1' src='https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80' />
                 </div>
                 <div className='img_container'>
-                    <div className='img_cont'>
-                        <img className='img' src='https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80' />
-                        <h1 className='img_header1'>ABCDEF</h1>
-                    </div>
-                    <div className='img_cont'>
-                        <img className='img' src='https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80' />
-                        <h1 className='img_header2'>ABCDEF</h1>
-                    </div>
-                    <div className='img_cont'>
-                        <img className='img' src='https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80' />
-                        <h1 className='img_header3'>ABCDEF</h1>
-                    </div>
-                    <div className='img_cont'>
-                        <img className='img' src='https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80' />
-                        <h1 className='img_header4'>ABCDEF</h1>
-                    </div>
+                    {
+                        info.map((i) => {
+                            return (
+                                <div className='img_cont'>
+                                    <img className='img' src={i.img} />
+                                    <h1 className='img_header'>{i.title}</h1>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
                 
+            </div>
+            <div className='quote'>
+                <h1 className='q'>BEST IN KAPURTHALA</h1>
             </div>
             <div className='aboutus_container'>
                 <h1 className='aboutus'>About Us</h1>
                 <p className='par'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Smart solutions is the best computer/laptop repair shops in Kapurthala. We repair hardware of computer and fix computers PCs, laptops, Printers, at affordable rates in Kapurthala. We also offer quality and on time repairs without wasting your valuable time.</p>
                 <p className='par'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We also offer Home & Business installations to ensure all your devices are connected and operating properly. We are open from Monday-Saturday and provide a NO FIX NO FEE SERVICE! We have over 20 years of experience fixing computers and guarantee a high level of service with 90% of repairs fixed within 48 hours.</p>
+            </div>
+            <div className='cards'>
+                <div className='spec_title'>
+                    <h1 className='aboutus1'>OUR SERVICES</h1>
+                </div>
+                {
+                    info.map(i => {
+                        return (
+                            <div className='card_cont' key={i.id}>
+                                <div className='card_img_container'>
+                                    <img className='card_img' src={i.img} />
+                                </div>
+                                <div className='card_info'>
+                                    <h1 className='card_header1'>{i.title}</h1>
+                                    <p className='card_p'>{i.info}</p>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
             </div>
         </div>
     )
